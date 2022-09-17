595d36767f6451389f4d8cd942b5875cad5648bd
paridhijha97 <109964493+paridhijha97@users.noreply.github.com>
2022-09-18 01:56:20 +0530

    Add files via upload

---
+++ luck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for value in {1..10};
+do
+
+echo $value "ALL THE BEST PARI"
+echo $value "WE BELIEVE IN YOU"
+echo $value "YOU CAN DO THIS"
+echo $value "YOU GOT THIS"
+echo $value "HWAITING"
+
+
+done;
